28949f9fc629df9e9903aa98b4915f4e1cdd431a
favstats <fabio.votta@gmail.com>
2025-04-03 15:37:48 +0200

    update

--- start.R
+++ start.R
@@ -1,100 +1,42 @@
+# if (Sys.info()[["effective_user"]] == "favstats" | Sys.info()[["effective_user"]] == "favoo") {
+source("get_audiences.R")
+# } else {
 
-# source("get_audiences.R")
+# install.packages("pacman")
+# }
 
-pacman::p_load(knitr, tidyverse, openxlsx, sf, rmarkdown)
+
+
+pacman::p_load(knitr, tidyverse, openxlsx, rmarkdown, rvest, shinydashboard, tidytable)
 # setwd("C:/Users/fabio/Dropbox/postdoc/microdashboards/wtm_iq/")
 # setwd("..")
-# getwd()
-
-source("cntry.R")
-
-all_dat <- readRDS("data/all_dat.rds")
-color_dat <- readRDS("data/color_dat.rds")
-
-
-
-if(read_lines("cntry.R") %>% length() > 5){
-  election_dat30 <- readRDS("data/election_dat30.rds")  %>%
-    select(-contains("party")) %>%
-    left_join(all_dat %>% select(page_id, party))
-}
-
-if(!exists("election_dat30")){
-  election_dat30 <- readRDS("data/election_dat30.rds")
-}
-
-
-raw <- election_dat30 %>%
-  # rename(internal_id = contains("page_id")) %>%
-  filter(is.na(no_data)) %>%
-  filter(sources == "wtm")
-
-if(nrow(raw)==0){
-
-  if(read_lines("cntry.R") %>% length() > 5){
-    election_dat30 <- election_dat30 %>%
-      rename(internal_id = contains("page_id")) %>%
-      filter(is.na(no_data)) %>%
-      drop_na(party) %>%
-      filter(party %in% color_dat$party)
+# print(getwd())
+render_it <- function(...) {
+  print(...)
+  thefile <- str_remove(..., "_site/") %>% str_replace("qmd", "html")
+  if(any(str_detect(..., "map"))){
+    if(cntryy == "NL"){
+      quarto::quarto_render(..., quiet = F)
+    }
   } else {
-    election_dat30 <- tibble()
+    # print(thefile)
+    quarto::quarto_render(..., quiet = F)
   }
-
-
-} else {
-  election_dat30 <- raw %>%
-    drop_na(party) %>%
-    filter(party %in% color_dat$party)
+  
 }
 
+render_it <- purrr::possibly(render_it, otherwise = NULL)
+dir("_site", full.names = T) %>% keep(~str_detect(.x, "qmd")) %>% keep(~str_detect(.x, "qmd")) %>% walk(render_it)
 
 
-# rstudioapi::jobRunScript("fbadlibrary.R")
-try({
+if (Sys.info()[["effective_user"]] == "favstats" | Sys.info()[["effective_user"]] == "favoo") {
+  # system("git pull")
+  # gert::git_pull()
+  # system("git add -A")
+  # system('git commit -m "update"')
+  # system("git push")
+  gert::git_add(".")
+  gert::git_commit("update")
+  gert::git_push(force = T)
+}
 
-
-
-
-  if(nrow(election_dat30)!=0){
-
-    # Sys.sleep(60*7)
-    all_dat <- readRDS("data/all_dat.rds")
-
-    write_lines(nrow(all_dat), file = "n_advertisers.txt")
-    render_it <- possibly(quarto::quarto_render, otherwise = NULL, quiet = F)
-    dir("_site", full.names = T) %>% keep(~str_detect(.x, "qmd")) %>%
-      # discard(~str_detect(.x, "location")) %>%
-      walk(render_it)
-
-    knitr::knit("README.Rmd")
-
-    rmarkdown::render("logs/overview.Rmd")
-
-    file.copy(from = "logs/overview.html", to = "docs/overview.html", overwrite = T)
-
-    unlink("node_modules", recursive = T, force = T)
-    unlink("out", recursive = T, force = T)
-
-  } else {
-
-    rmarkdown::render("logs/index.Rmd")
-
-    file.copy(from = "logs/index.Rmd", to = "docs/index.html", overwrite = T)
-
-    unlink("node_modules", recursive = T, force = T)
-    unlink("out", recursive = T, force = T)
-
-  }
-
-
-
-
-
-})
-
-
-# gert::git_pull()
-gert::git_add(".")
-gert::git_commit("update")
-gert::git_push(force = T)
